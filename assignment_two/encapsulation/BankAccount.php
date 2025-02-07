1476e2d42a70ddc5d42dfd550bf8f51e3a0bca17
Cyril Tagoe <96398158+Cyril-Nii@users.noreply.github.com>
2025-02-07 02:59:28 +0000

    Update BankAccount.php

--- assignment_two/encapsulation/BankAccount.php
+++ assignment_two/encapsulation/BankAccount.php
@@ -3,14 +3,6 @@
 namespace banking_system;
 
 class BankAccount {
-<<<<<<< HEAD
-/* -  accountNumber (string): The account number of the bank account 
--  accountHolder (string): The name of the account holder. 
--  balance (float): The current balance in the account (must be ≥ 0.0). 
--  interestRate (float): The annual interest rate for the account (must be between 0.0 and 
-1.0).*/
-=======
->>>>>>> f247f56 (Some corrections made)
 
 private string $accountNumber;
 private string $accountHolder;
@@ -75,21 +67,10 @@ public function setInterestRate(float $interestRate): void {
     $this->interestRate = $interestRate;
 }
 
-
-<<<<<<< HEAD
-// deposit(float $amount): Adds the specified amount to the balance.
-=======
-
->>>>>>> f247f56 (Some corrections made)
 public function deposit(float $amount): void{
     $this->balance += $amount;
 }
 
-<<<<<<< HEAD
-// withdraw(float $amount): Deducts the specified amount from the balance(if sufficient funds are available)
-=======
-
->>>>>>> f247f56 (Some corrections made)
 public function withdraw(float $amount): void{
     if($this->balance >= $amount) {
         $this->balance -= $amount;
@@ -99,11 +80,6 @@ public function withdraw(float $amount): void{
     }
 }
 
-<<<<<<< HEAD
-// calculateInterest: Returns the annual interest earned using the formula: balance * interestRate.
-=======
-
->>>>>>> f247f56 (Some corrections made)
 public function calculateInterest(): float{
     return $this->balance * $this->interestRate;
 }
@@ -113,17 +89,6 @@ public function calculateInterest(): float{
 
 }
 
-<<<<<<< HEAD
-/* A. Implement the BankAccount class with encapsulation in PHP. 
-B. Create an instance of the BankAccount class. 
-C. Update the account balance and display updated details 
-D. Calculate and display annual interest earned
-*/
-
-// New instance of the Bank 
-=======
-// New instance of the BankAccount class
->>>>>>> f247f56 (Some corrections made)
 $account = new BankAccount("223505990", "Cyril Tagoe", 1000.0, 0.05);
 
 // Updating the account balance
