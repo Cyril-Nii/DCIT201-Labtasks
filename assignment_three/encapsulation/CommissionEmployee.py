bb0e32bae8c9a01c13d8b27f15915ffc42df3aa4
Cyril-Nii <cyriltagoe0@gmail.com>
2025-02-10 16:20:13 +0000

    Update to python encapsulation

--- assignment_three/encapsulation/CommissionEmployee.py
+++ assignment_three/encapsulation/CommissionEmployee.py
@@ -1,11 +1,11 @@
 class CommisisonEmployee:
     
     def __init__(self, first_name, last_name, social_security_number, gross_sales, commission_rate):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.social_security_number = social_security_number
-        self.gross_sales = gross_sales
-        self.commission_rate = commission_rate
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__social_security_number = social_security_number
+        self.__gross_sales = gross_sales
+        self.__commission_rate = commission_rate
         
     # Getters and Setters
     def get_first_name(self):
